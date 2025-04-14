89d96e58d082800f11069eba3203f02e56222690
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-04-14 11:41:37 +0200

    🚩🗃️ feat(db): create and configure file for database (migrations, factories, seeders)

---
+++ database/factories/QuizFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Quiz>
+ */
+class QuizFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "title" => fake()->title(),
+            "description" => fake()->paragraph(),
+            "is_public" => fake()->boolean(),
+            "user_id" => fake()->numberBetween(1, 20),
+            "level_id" => fake()->numberBetween(1, 3),
+        ];
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory(20)->create();
+
+        $adminDetails = [
+            "username" => env("ADMIN_USERNAME", "default_username"),
+            "firstname" => env("ADMIN_FIRSTNAME", "default_firstname"),
+            "lastname" => env("ADMIN_LASTNAME", "default_lastname"),
+            "email" => env("ADMIN_EMAIL", "default_email"),
+            "password" => bcrypt(env("ADMIN_PASSWORD", "default_password")),
+            "role_id" => 1,
+        ];
+
+        User::create($adminDetails);
+
+        echo "\nAdmin user created with the following details:\n\n";
+        foreach ($adminDetails as $key => $value) {
+            echo "$key: $value\n";
+        }
+    }
+}
