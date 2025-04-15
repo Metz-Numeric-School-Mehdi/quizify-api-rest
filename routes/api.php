ea23e71b720dec55e9fe89677397032a14f2068d
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-04-15 11:50:23 +0200

    ✨🚚 feat(routes): add route for quizzes request

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\QuizController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 include base_path("routes/auth.php");
+
+Route::get("/quizzes", [QuizController::class, "index"]);
+Route::post("/quizzes", [QuizController::class, "store"])->middleware("auth:sanctum");
+Route::delete("/quizzes/{id}", [QuizController::class, "destroy"])->middleware("auth:sanctum");
+Route::put("/quizzes/{id}", [QuizController::class, "update"])->middleware("auth:sanctum");
 
 Route::get("/user", function (Request $request) {
     return $request->user();
