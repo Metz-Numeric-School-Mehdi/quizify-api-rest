27413852e77162d94d44b80b4326ef9f053b0172
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-05-31 23:48:29 +0200

    🚀 feat(controller): create ScoreController with CRUD methods for scores

--- app/Models/Score.php
+++ app/Models/Score.php
@@ -10,4 +10,14 @@ class Score extends Model
     use SoftDeletes;
 
     protected $fillable = ["user_id", "quiz_id", "score"];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function quiz()
+    {
+        return $this->belongsTo(Quiz::class);
+    }
 }
