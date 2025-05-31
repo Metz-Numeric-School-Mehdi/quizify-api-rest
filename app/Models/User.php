5a5c6f185d53b1fc817bf0420828ac724b0f61c1
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-05-31 23:47:56 +0200

    🚀 feat(model): update User model to include team_id and organization_id, and adjust fillable attributes

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,27 +15,20 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, SoftDeletes, HasApiTokens;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         "username",
         "firstname",
         "lastname",
         "email",
         "password",
-        "role",
+        "role_id",
         "profile_photo",
+        "team_id",
+        "organization_id",
     ];
 
-    /**
-     * Get the role associated with this user
-     */
     public function role(): BelongsTo
     {
         return $this->BelongsTo(Role::class);
@@ -56,18 +49,28 @@ class User extends Authenticatable
         return $this->belongsToMany(Quiz::class);
     }
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function organization(): BelongsTo
+    {
+        return $this->belongsTo(Organization::class);
+    }
+
+    public function questionResponses(): HasMany
+    {
+        return $this->hasMany(QuestionResponse::class);
+    }
+
+    public function scores(): HasMany
+    {
+        return $this->hasMany(Score::class);
+    }
+
     protected $hidden = ["password", "remember_token"];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
