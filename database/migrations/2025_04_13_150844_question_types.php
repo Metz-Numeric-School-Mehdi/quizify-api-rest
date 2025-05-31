820641a6d3736f3c2e09d79d8cb4a8ac56872c38
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-05-31 23:48:09 +0200

    🚀 feat(controller): add QuestionTypeController with CRUD methods for question types

--- database/migrations/2025_04_13_150844_question_types.php
+++ database/migrations/2025_04_13_150844_question_types.php
@@ -5,23 +5,17 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create("question_types", function (Blueprint $table) {
+        Schema::create('question_types', function (Blueprint $table) {
             $table->id();
-            $table->string("name")->unique();
+            $table->string('name');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists("question_types");
+        Schema::dropIfExists('question_types');
     }
 };
