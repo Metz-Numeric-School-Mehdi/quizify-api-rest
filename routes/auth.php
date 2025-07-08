66b0dc521928128500707e85f35f11b11ce3ba4c
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-07-08 10:35:40 +0200

    🚩 feat: add verify method

--- routes/auth.php
+++ routes/auth.php
@@ -9,4 +9,10 @@ Route::prefix("auth")->group(function () {
     Route::post("/signin", [AuthController::class, "signIn"]);
     Route::post("/signup", [AuthController::class, "signUp"]);
     Route::get("/signout", [AuthController::class, "signOut"])->middleware("auth:sanctum");
+    Route::get('/verify', function (\Illuminate\Http\Request $request) {
+        return response()->json([
+            'authenticated' => $request->user() ? true : false,
+            'user' => $request->user()
+        ]);
+    })->middleware('auth:sanctum');
 });
