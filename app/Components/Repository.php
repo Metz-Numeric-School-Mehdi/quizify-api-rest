0de4a9fdc0645aec6662f74c5dd5474441ddcc5c
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-07-31 22:50:44 +0200

    🚩🌱 feat: create abstract class Repository

---
+++ app/Components/Repository.php
@@ -0,0 +1,60 @@
+<?php
+namespace App\Components;
+
+use App\Repositories\Quiz\RepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class Repository implements RepositoryInterface
+{
+    /**
+     * The Eloquent model instance.
+     *
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * Repository constructor.
+     *
+     * @param Model $model
+     */
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function index()
+    {
+        return $this->model->all();
+    }
+
+    public function show($id)
+    {
+        return $this->model->findOrFail($id);
+    }
+
+    public function submit($request, $quizId) {}
+    public function store($request) {}
+    public function update($request, $id) {}
+    public function destroy($request, $id) {}
+    public function storeAttempt($request, $quizId) {}
+
+    // protected function applyFilter(array $params)
+    // {
+    //     $query = $this->model->newQuery();
+    // }
+
+    // protected function setFilters(array &$params, Builder $query): void
+    // {
+    //     if (isset($params["filters"])) {
+    //         foreach ($params["filters"] as $key => $value) {
+    //             if (is_array($value)) {
+    //                 $query->whereIn($key, $value);
+    //             } else {
+    //                 $query->where($key, $value);
+    //             }
+    //         }
+    //         unset($params["filters"]);
+    //     }
+    // }
+}
