8aa4ef2d897c4a5ab74d89ecba8947e5f93faacb
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-08-04 10:51:08 +0200

    🚩🌐 feat: create french validation message

---
+++ resources/lang/fr/validation.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    "required" => "Le champ :attribute est requis.",
+    "string" => "Le champ :attribute doit être une chaîne de caractères.",
+    "max" => [
+        "string" => "Le champ :attribute ne doit pas dépasser :max caractères.",
+    ],
+    "boolean" => "Le champ :attribute doit être vrai ou faux.",
+    "integer" => "Le champ :attribute doit être un nombre entier.",
+    "exists" => "Le champ :attribute sélectionné est invalide.",
+    "in" => "Le champ :attribute doit être l’une des valeurs suivantes : :values.",
+    "unique" => "Ce :attribute existe déjà.",
+    "min" => [
+        "numeric" => "Le champ :attribute doit être au moins égal à :min.",
+    ],
+
+    "attributes" => [
+        "title" => "titre",
+        "slug" => "slug",
+        "description" => "description",
+        "is_public" => "visibilité",
+        "level_id" => "niveau",
+        "status" => "statut",
+        "user_id" => "utilisateur",
+        "duration" => "durée",
+        "pass_score" => "score de réussite",
+        "thumbnail" => "miniature",
+        "category_id" => "catégorie",
+    ],
+];
