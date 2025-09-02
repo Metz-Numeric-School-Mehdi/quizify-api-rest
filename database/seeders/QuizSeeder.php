5f8cff929596537400a5357551c9f7ce0b10ed62
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-09-02 11:22:23 +0200

    🌱 feat: add additional quizzes for PHP, JavaScript, databases, and React in QuizSeeder

--- database/seeders/QuizSeeder.php
+++ database/seeders/QuizSeeder.php
@@ -15,7 +15,7 @@ class QuizSeeder extends Seeder
         Quiz::withoutSyncingToSearch(function () {
             Quiz::create([
                 "title" => "Quiz Laravel",
-                "slug" => "quiz-laravel",
+                "slug" => "quiz-laravel", 
                 "description" => "Testez vos connaissances sur Laravel.",
                 "is_public" => true,
                 "level_id" => 1,
@@ -23,6 +23,62 @@ class QuizSeeder extends Seeder
                 "user_id" => 1,
                 "duration" => 30,
                 "pass_score" => 70,
+                "thumbnail" => null,
+                "category_id" => 1,
+            ]);
+
+            Quiz::create([
+                "title" => "Quiz PHP Avancé",
+                "slug" => "quiz-php-avance",
+                "description" => "Questions avancées sur PHP et la programmation orientée objet.",
+                "is_public" => true,
+                "level_id" => 2,
+                "status" => "published", 
+                "user_id" => 1,
+                "duration" => 45,
+                "pass_score" => 75,
+                "thumbnail" => null,
+                "category_id" => 1,
+            ]);
+
+            Quiz::create([
+                "title" => "Quiz JavaScript ES6+",
+                "slug" => "quiz-javascript-es6",
+                "description" => "Maîtrisez les nouvelles fonctionnalités de JavaScript ES6 et plus.",
+                "is_public" => true,
+                "level_id" => 2,
+                "status" => "published",
+                "user_id" => 1,
+                "duration" => 40,
+                "pass_score" => 70,
+                "thumbnail" => null,
+                "category_id" => 1,
+            ]);
+
+            Quiz::create([
+                "title" => "Quiz Base de Données",
+                "slug" => "quiz-base-donnees",
+                "description" => "Questions sur MySQL, PostgreSQL et les concepts de base de données.",
+                "is_public" => false,
+                "level_id" => 1,
+                "status" => "draft",
+                "user_id" => 1,
+                "duration" => 35,
+                "pass_score" => 65,
+                "thumbnail" => null,
+                "category_id" => 1,
+            ]);
+
+            Quiz::create([
+                "title" => "Quiz React Fundamentals",
+                "slug" => "quiz-react-fundamentals",
+                "description" => "Testez vos connaissances de base sur React et les composants.",
+                "is_public" => true,
+                "level_id" => 1,
+                "status" => "published",
+                "user_id" => 1,
+                "duration" => 25,
+                "pass_score" => 60,
                 "thumbnail" => null,
                 "category_id" => 1,
             ]);
